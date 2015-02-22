e472901f6b9501db72cf3aa230df8b0c022d9d37
Riven8192 <jgo@kataf.nl>
2015-02-22 21:31:21 +0100

    added support for directory classpath entries in StructPacker

--- src/net/indiespot/struct/StructPacker.java
+++ src/net/indiespot/struct/StructPacker.java
@@ -11,9 +11,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.jar.JarInputStream;
 import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
@@ -27,16 +29,18 @@ public class StructPacker {
 			System.out.println("Usage:");
 			System.out.println(" -structdef [path]  (any number of times)");
 			System.out.println(" -jar [path]        (any number of times)");
+			System.out.println(" -dir [path]        (any number of times)");
 			System.out.println(" -out [path]        (once)");
 			System.out.println();
 			System.out.println("Example:");
-			System.out.println("StructPacker -structdef [path1] -jar [path2] -jar [path3] -jar [path4] -structdef [path5] -jar [path6] -out [path7]");
+			System.out.println("StructPacker -structdef [path0] -dir [path1] -jar [path2] -jar [path3] -jar [path4] -structdef [path5] -jar [path6] -out [path7]");
 			return;
 		}
 
-		// process commandline arguments
+		// process command-line arguments
 		List<String> structdefFiles = new ArrayList<>();
 		List<String> inJarFiles = new ArrayList<>();
+		List<String> inDirPaths = new ArrayList<>();
 		String outJarFile = "./libstruct-cp-prefix.jar";
 
 		for (int i = 0; i < args.length; i += 2) {
@@ -47,16 +51,27 @@ public class StructPacker {
 				structdefFiles.add(val);
 			else if (key.equals("-jar"))
 				inJarFiles.add(val);
+			else if (key.equals("-dir"))
+				inDirPaths.add(val);
 			else if (key.equals("-out"))
 				outJarFile = val;
 			else
 				throw new IllegalStateException();
 		}
 
-		System.out.println("processing " + inJarFiles.size() + " jar files...");
+		final Set<String> ignores = new HashSet<>();
+		{
+			ignores.add("net/indiespot/struct/codegen/StructGenericsSourcecodeGenerator");
+			ignores.add("net/indiespot/struct/codegen/templates/List");
+			ignores.add("net/indiespot/struct/codegen/templates/Block");
+			ignores.add("test/net/indiespot/struct/StructTest$TestCollectionAPI");
+			ignores.add("test/net/indiespot/struct/StructTest$TestDuplicateOverloadedMethod");
+		}
 
 		// gather all classnames and their bytecode
 		final Map<String, byte[]> fqcn2bytecode = new HashMap<>();
+
+		System.out.println("processing " + inJarFiles.size() + " jar files...");
 		for (String jar : inJarFiles) {
 			System.out.println("\t processing jar file: " + jar);
 			try (JarInputStream jis = new JarInputStream(new FileInputStream(jar))) {
@@ -73,11 +88,7 @@ public class StructPacker {
 					if (fqcn.startsWith("/"))
 						fqcn = fqcn.substring(1);
 
-					if (fqcn.startsWith("net/indiespot/struct/codegen/"))
-						continue;
-					if (fqcn.equals("test/net/indiespot/struct/StructTest$TestCollectionAPI"))
-						continue;
-					if (fqcn.equals("test/net/indiespot/struct/StructTest$TestDuplicateOverloadedMethod"))
+					if (ignores.contains(fqcn))
 						continue;
 
 					byte[] bytecode = readFully(jis);
@@ -86,6 +97,39 @@ public class StructPacker {
 					fqcn2bytecode.put(fqcn, bytecode);
 				}
 			}
+		}
+
+		System.out.println("processing " + inDirPaths.size() + " dir paths...");
+		for (String dir : inDirPaths) {
+			System.out.println("\t processing dir path: " + dir);
+
+			traverse(new File(dir), "", new Callback() {
+				@Override
+				public void onFile(File file, String relative) {
+					if (!relative.endsWith(".class"))
+						return;
+
+					String fqcn = relative;
+					fqcn = fqcn.substring(0, fqcn.length() - ".class".length());
+					fqcn = fqcn.replace('\\', '/');
+					if (fqcn.startsWith("/"))
+						fqcn = fqcn.substring(1);
+
+					if (ignores.contains(fqcn))
+						return;
+
+					byte[] bytecode;
+					try (FileInputStream fis = new FileInputStream(file)) {
+						bytecode = readFully(fis);
+					} catch (IOException exc) {
+						throw new IllegalStateException(exc);
+					}
+
+					if (fqcn2bytecode.containsKey(fqcn) && !Arrays.equals(fqcn2bytecode.get(fqcn), bytecode))
+						throw new IllegalStateException("duplicate fqcn [" + fqcn + "] with different bytecode");
+					fqcn2bytecode.put(fqcn, bytecode);
+				}
+			});
 		}
 
 		if (!fqcn2bytecode.containsKey("net/indiespot/struct/cp/Struct"))
@@ -137,6 +181,21 @@ public class StructPacker {
 		System.out.println("\t Full path: " + new File(outJarFile).getCanonicalPath());
 		System.out.println("\t Put generated file as first entry in the classpath of your application, so that rewritten classes take precedence.");
 		System.out.println("Done.");
+	}
+
+	private static interface Callback {
+		public void onFile(File file, String relative);
+	}
+
+	private static void traverse(File dir, String relative, Callback callback) {
+
+		for (File file : dir.listFiles()) {
+			if (file.isDirectory()) {
+				traverse(file, relative + file.getName() + "/", callback);
+			} else {
+				callback.onFile(file, relative + file.getName());
+			}
+		}
 	}
 
 	private static byte[] readFully(InputStream is) throws IOException {
